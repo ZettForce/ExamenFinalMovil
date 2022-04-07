0dc1004096e00f4bf4686a73f3a0e8b267cf7ee0
ZettForce <83797376+ZettForce@users.noreply.github.com>
2022-04-06 19:45:03 -0700

    v1

--- examenfinalmovil/lib/main.dart
+++ examenfinalmovil/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:examenfinalmovil/pages/rick_page.dart';
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
